45c04ff74227efcc8cab4c3fa43056f52b21328a
Colm Murphy <colmmurphy016@gmail.com>
2022-11-26 16:23:23 +0000

    making it object oriented

---
+++ main_oo.py
@@ -0,0 +1,24 @@
+import sys
+import pygame
+
+
+class Game(object):
+    def __init__(self):
+        pass
+
+    def run_game(self):
+        pass
+
+
+class ShipState(object):
+    def __init__(self):
+        pass
+
+
+def main():
+    game = Game()
+    game.run_game()
+
+
+if __name__ == "__main__":
+    main()
